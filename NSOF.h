afe6c91a66b5498adc991c148caa7ba673c08854
Matthias Melcher <github@matthiasm.com>
2024-07-05 17:45:38 +0200

    Line endings

--- NSOF.h
+++ NSOF.h
@@ -1,1 +1,20 @@
-@interface FDObjectReader : NSObject{	NSFileHandle *	pipe;	NSData *			data;	CPrecedents *	precedents;	char				buf[256];}- (void) dealloc;- (Ref) read: (NSString *) path;- (Ref) readobject;- (UInt8) readbyte;- (UInt16) readhalfword;- (SInt32) readlong;- (SInt32) readxlong;@end
+
+@interface FDObjectReader : NSObject
+{
+	NSFileHandle *	pipe;
+	NSData *			data;
+	CPrecedents *	precedents;
+	char				buf[256];
+}
+
+- (void) dealloc;
+- (Ref) read: (NSString *) path;
+
+- (Ref) readobject;
+- (UInt8) readbyte;
+- (UInt16) readhalfword;
+- (SInt32) readlong;
+- (SInt32) readxlong;
+
+@end
+
